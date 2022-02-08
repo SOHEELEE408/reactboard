9fff887988bdfc6a699a7b554f930132cfa8e658
minseok <minseok7407@naver.com>
2022-02-08 10:24:42 +0900

    rollback을 위한 커밋

--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -4,10 +4,8 @@ module.exports=function(app){
     app.use(
         '/api',
         createProxyMiddleware({
-            target:'http://localhost:8080',
+            target:'https://react-board-w-boot.com.herokuapp.com',
             changeOrigin:true,
         })
     )
 }
-
-//'https://react-board-w-boot.com.herokuapp.com'
